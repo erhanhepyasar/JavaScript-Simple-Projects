a5a68a5636d344d30225de5c1f355af733ace400
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2019-09-25 21:05:31 +0300

    commit

--- 8-digitalclock_old/script.js
+++ 8-digitalclock_old/script.js
@@ -1,6 +1,7 @@
 const clock = document.querySelector('.container');
 
-setInterval(myTimer, 1000);
+myTimer(); // for immediate appearance of time as soon as page loads
+setInterval(myTimer, 1000); // interval starts after given time
 
 function myTimer(){
     clock.innerHTML = new Date().toLocaleTimeString();
